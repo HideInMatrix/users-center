a3ea8dd6b9ff5374b6bff4943dd5023f22b7b850
HideInMatrix <davimorgancreate@gmail.com>
2024-07-16 11:22:25 +0800

    feat:增加跨域配置

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  // 允许来自 http://localhost:3000 的请求
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true, // 允许发送 Cookie
+  });
   await app.listen(7000);
 }
 bootstrap();
